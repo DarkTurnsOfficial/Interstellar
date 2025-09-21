31c63db2d126e507ad51fb864175a0febf3c5ac4
Kayden <117483970+DarkTurnsOfficial@users.noreply.github.com>
2025-09-21 13:30:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,20 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Jayden: "CHANGE PASSWORD!!!! @*83838578591230093127932189",
+    Robby: "Robby0401",
+    Jacob: "Strongpassword@jb",
+    Jaxson: "Mountindew13",
+    Cylis: "Cylis164341",
+    Carter: "Pizza",
+    Owner: "4523",
+    Levi: "Fucknigas12",
+    Mason: "Hasard26!",
+    River: "Something3655",
+    Sylis: "Therealguy",
+    Drake: "67mango",
+    Evan: "164093ea",
   },
 };
 
